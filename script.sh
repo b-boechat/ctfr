91dd616755ccfcb3e3c5c70a6ffeded8f4152778
Bernardo Boechat <bernardoant@poli.ufrj.br>
2024-04-04 21:28:50 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 project_name="tfrc"
+python_versions="cp310-cp310 cp311-cp311 cp312-cp312"
 
 python_install_paths="/opt/python/cp310-cp310/bin/python3 /opt/python/cp311-cp311/bin/python3 /opt/python/cp312-cp312/bin/python3"
+python_location="/opt/python"
+python_bin="bin/python3"
 
 cd /io
-for python_path in $python_install_paths; do
-    #echo $project_name
+for version in $python_versions; do
+    python_path=$python_location/$version/$python_bin
     PYTHON_EXEC=$python_path make wheel
-    auditwheel repair dist/$(tfrc)*linux*.whl
+    auditwheel repair dist/$(project_name)*linux*.whl
     make clean
 done
