52481ef705c0140132719b6bd31aaf359c874e8a
Bernardo Boechat <bernardoant@poli.ufrj.br>
2024-04-09 21:06:35 -0300

    Start building core combination-method-calling functionality.

---
+++ src/tfrc/exception.py
@@ -0,0 +1,8 @@
+class Error(Exception):
+    pass
+
+class InvalidRepresentationType(Error):
+    pass
+
+class InvalidCombinationMethod(Error):
+    pass
---
+++ src/tfrc/methods/methods.py
@@ -0,0 +1,20 @@
+from . import _fls_wrapper, _swgm_wrapper, _lt_wrapper
+from tfrc.exception import InvalidCombinationMethod
+
+methods_dict = {
+    "swgm": {
+        "function": _swgm_wrapper
+    },
+    "fls": {
+        "function": _swgm_wrapper
+    },
+    "lt": {
+        "function": _lt_wrapper
+    },
+}
+
+def _get_method_function(key):
+    try:
+        return methods_dict[key][function]
+    except KeyError:
+        raise InvalidCombinationMethod(f"Invalid combination method: {key}")
