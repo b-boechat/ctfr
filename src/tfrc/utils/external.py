747401510f940ac5dd417435ef7214ff4c477cef
Bernardo Boechat <bernardoant@poli.ufrj.br>
2024-04-28 17:04:53 -0300

    Add specshow to external utils.

--- src/tfrc/utils/external.py
+++ src/tfrc/utils/external.py
@@ -1,4 +1,5 @@
 import librosa
+from librosa.display import specshow as specshow_librosa
 import numpy as np
 #from librosa.display import specshow
 
@@ -17,3 +18,9 @@ def stft_spec(signal, *, n_fft=2048, hop_length=None, win_length=None, window='h
 
 def cqt_spec(signal, **kwargs):
     return np.square(np.abs(cqt(signal, **kwargs), dtype=np.double))
+
+# Librosa specshow with added x_lim and y_lim arguments.
+def specshow(data, *, x_coords=None, y_coords=None, x_axis=None, y_axis=None, sr=22050, hop_length=512, n_fft=None, win_length=None, fmin=None, fmax=None, tempo_min=16, tempo_max=480, tuning=0.0, bins_per_octave=12, key='C:maj', Sa=None, mela=None, thaat=None, auto_aspect=True, htk=False, unicode=True, intervals=None, unison=None, ax=None, **kwargs):
+    return specshow_librosa(data, x_coords=x_coords, y_coords=y_coords, x_axis=x_axis, y_axis=y_axis, sr=sr, hop_length=hop_length, n_fft=n_fft, win_length=win_length, fmin=fmin, fmax=fmax, tempo_min=tempo_min, tempo_max=tempo_max, tuning=tuning, bins_per_octave=bins_per_octave, key=key, Sa=Sa, mela=mela, thaat=thaat, auto_aspect=auto_aspect, htk=htk, unicode=unicode, intervals=intervals, unison=unison, ax=ax, **kwargs)
+
+    
