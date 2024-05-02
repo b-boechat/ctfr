a84b4e3e6efed4b268740b0f761cf57d5665a33a
b-boechat <bernardoant@poli.ufrj.br>
2024-05-02 20:39:01 -0300

    Add version function.

--- src/tfrc/meta.py
+++ src/tfrc/meta.py
@@ -1,8 +1,12 @@
 from .methods.methods_utils import _get_method_citation, get_method_name
 from .exception import CitationNotImplementedError
+from tfrc import __version__
 
 def cite():
     raise CitationNotImplementedError("Package citation not implemented")
 
 def cite_method(method, mode=None):
     print(_get_method_citation(method, mode=mode))
+
+def version():
+    print(f"Version: {__version__}")
