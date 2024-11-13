6fe2bb9fa0971b7f5ea35dde133b6448ffe5691a
Bernardo Boechat <bernardoant@poli.ufrj.br>
2024-11-13 14:34:01 -0300

    Fix method function exports.

--- src/ctfr/__init__.py
+++ src/ctfr/__init__.py
@@ -24,7 +24,7 @@ def _from_audio_function_export(key):
     if not validate_function_name(function_name):
         _warn(f"Function name already exists in module ctfr.methods and thus was not built: {function_name}.", FunctionNotBuiltWarning)
     #globals()[function_name] = lambda signal, **kwargs: ctfr(signal, method = key, **kwargs)
-    setattr(methods, function_name, lambda signal, **kwargs: ctfr(signal, method = key, **kwargs))
+    setattr(methods, function_name, lambda signal, sr, **kwargs: ctfr(signal, sr = sr, method = key, **kwargs))
 
 def _from_specs_function_export(key):
     """Export a method function that takes an iterable of spectrograms as input."""
@@ -32,7 +32,7 @@ def _from_specs_function_export(key):
     if not validate_function_name(function_name):
         _warn(f"Function name already exists in module ctfr.methods and thus was not built: {function_name}.", FunctionNotBuiltWarning)
     #globals()[function_name] = lambda specs_tensor, **kwargs: ctfr_from_specs(specs_tensor, method = key, **kwargs)
-    setattr(methods, function_name, lambda signal, **kwargs: ctfr(signal, method = key, **kwargs))
+    setattr(methods, function_name, lambda specs_tensor, **kwargs: ctfr_from_specs(specs_tensor, method = key, **kwargs))
 
 def validate_function_name(function_name):
     return not function_name in globals()
