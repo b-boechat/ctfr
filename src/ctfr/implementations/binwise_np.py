9f327412a0f2040f0df8e7bf6244bd6e74981003
Bernardo Boechat <bernardoant@poli.ufrj.br>
2024-11-16 18:27:38 -0300

    Add binwise gmean.

--- src/ctfr/implementations/binwise_np.py
+++ src/ctfr/implementations/binwise_np.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.stats import gmean
 
 def _mean_wrapper(X):
     return np.mean(X, axis=0)
@@ -8,3 +9,6 @@ def _median_wrapper(X):
 
 def _min_wrapper(X):
     return np.min(X, axis=0)
+
+def _gmean_wrapper(X):
+    return gmean(X, axis=0)
