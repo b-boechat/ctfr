99ce481f34623d1faea3d2aa436192ae932bde4a
Bernardo Boechat <bernardoant@poli.ufrj.br>
2025-03-13 21:13:29 -0300

    Update running time script.

--- running_time/script.py
+++ running_time/script.py
@@ -1,55 +1,45 @@
 import ctfr
+import sys
 
-def load_placeholder():
+def load_test_signal():
 
-    signal, _ = ctfr.load("running_time/audio/placeholder.wav")
+    signal, sr = ctfr.load("running_time/audio/guitarset_sample.wav", sr=22050, offset=5.0, duration=4.0)
 
     n_fft = 2048
     hop_length = 256
-
 
     spec_1 = ctfr.stft_spec(signal, win_length=512, n_fft=n_fft, hop_length=hop_length)
     spec_2 = ctfr.stft_spec(signal, win_length=1024, n_fft=n_fft, hop_length=hop_length)
     spec_3 = ctfr.stft_spec(signal, win_length=2048, n_fft=n_fft, hop_length=hop_length)
 
-    assert spec_1.shape == spec_2.shape
-    assert spec_1.shape == spec_3.shape
-
     return spec_1, spec_2, spec_3
 
-
-def time_method(load_function, method, num_iter=5, **kwargs):
-    spec_1, spec_2, spec_3 = load_function()
-
+def time_method(specs, method, num_iter=5, **kwargs):
     total_time = 0.0
     for i in range(num_iter):
-        swgm_spec, elapsed_time = ctfr.ctfr_from_specs((spec_1, spec_2, spec_3), method=method, **kwargs)
+        swgm_spec, elapsed_time = ctfr.ctfr_from_specs(specs, method=method, **kwargs)
         total_time += elapsed_time
     average_time = total_time / num_iter
+    return average_time
 
-    print(f"Method {method} - Averaged elapsed time: {average_time}")
+
+def time_all_pipeline(num_iter):
+
+    specs = load_test_signal()
+
+    print("Execution time for each method and implementation:")
+    
+    print("\n==== SWGM ====\n")
+    print(f"Baseline: {time_method(specs, method='baseline_swgm', num_iter=5):0.3f} s")
+    print(f"ctfr: {time_method(specs, method='swgm'):0.3f} s")
+
+    print("\n==== LT ====\n")
+    print(f"Baseline: {time_method(specs, method='baseline_lt', num_iter=5):0.3f} s")
+    print(f"ctfr: {time_method(specs, method='lt', num_iter=5):0.3f} s")
 
 if __name__ == "__main__":
-    time_method(
-        load_function=load_placeholder,
-        method='swgm',
-        num_iter=5
-    )
-
-    time_method(
-        load_function=load_placeholder,
-        method='baseline_swgm',
-        num_iter=5
-    )
-
-    time_method(
-        load_function=load_placeholder,
-        method='lt',
-        num_iter=5
-    )
-
-    time_method(
-        load_function=load_placeholder,
-        method='baseline_lt',
-        num_iter=5
-    )
+    if len(sys.argv) == 2:
+        num_iter = int(sys.argv[1])
+    else:
+        num_iter = 5
+    time_all_pipeline(num_iter)
