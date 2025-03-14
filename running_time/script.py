b6e2ab30f9499785f2d37586de819e35c17b898a
Bernardo Boechat <bernardoant@poli.ufrj.br>
2025-03-13 21:53:18 -0300

    Update script.py

--- running_time/script.py
+++ running_time/script.py
@@ -1,5 +1,6 @@
 import ctfr
 import sys
+import numpy as np
 
 def load_test_signal():
 
@@ -20,7 +21,7 @@ def time_method(specs, method, num_iter=5, **kwargs):
         swgm_spec, elapsed_time = ctfr.ctfr_from_specs(specs, method=method, **kwargs)
         total_time += elapsed_time
     average_time = total_time / num_iter
-    return average_time
+    return swgm_spec, average_time
 
 
 def time_all_pipeline(num_iter):
@@ -28,14 +29,24 @@ def time_all_pipeline(num_iter):
     specs = load_test_signal()
 
     print("Execution time for each method and implementation:")
+
+    print("\n==== Binwise minimum ====\n")
+    _, average_time_ctfr = time_method(specs, method='min', num_iter=num_iter)
+    print(f"ctfr: {average_time_ctfr:0.3f} s")
     
     print("\n==== SWGM ====\n")
-    print(f"Baseline: {time_method(specs, method='baseline_swgm', num_iter=5):0.3f} s")
-    print(f"ctfr: {time_method(specs, method='swgm'):0.3f} s")
+    cspec_base, average_time_base = time_method(specs, method='baseline_swgm', num_iter=num_iter)
+    print(f"Baseline: {average_time_base:0.3f} s")
+    cspec_ctfr, average_time_ctfr = time_method(specs, method='swgm', num_iter=num_iter)
+    print(f"ctfr: {average_time_ctfr:0.3f} s")
+    assert np.allclose(cspec_base, cspec_ctfr)
 
     print("\n==== LT ====\n")
-    print(f"Baseline: {time_method(specs, method='baseline_lt', num_iter=5):0.3f} s")
-    print(f"ctfr: {time_method(specs, method='lt', num_iter=5):0.3f} s")
+    cspec_base, average_time_base = time_method(specs, method='baseline_lt', num_iter=num_iter)
+    print(f"Baseline: {average_time_base:0.3f} s")
+    cspec_ctfr, average_time_ctfr = time_method(specs, method='lt', num_iter=num_iter)
+    print(f"ctfr: {average_time_ctfr:0.3f} s")
+    assert np.allclose(cspec_base, cspec_ctfr)
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
