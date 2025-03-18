193ab3df998d17ee70bc8aecd7ad8349385ea5bc
Bernardo Boechat <bernardoant@poli.ufrj.br>
2025-03-18 14:45:28 -0300

    Update script with relative times.

--- running_time/script.py
+++ running_time/script.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def load_test_signal():
 
-    signal, sr = ctfr.load("running_time/audio/guitarset_sample.wav", sr=22050, offset=5.0, duration=4.0)
+    signal, _ = ctfr.load("running_time/audio/guitarset_sample.wav", sr=22050, offset=5.0, duration=4.0)
 
     n_fft = 2048
     hop_length = 256
@@ -13,7 +13,9 @@ def load_test_signal():
     spec_2 = ctfr.stft_spec(signal, win_length=1024, n_fft=n_fft, hop_length=hop_length)
     spec_3 = ctfr.stft_spec(signal, win_length=2048, n_fft=n_fft, hop_length=hop_length)
 
-    return spec_1, spec_2, spec_3
+    print(f"Specs shape: {spec_1.shape}, {spec_2.shape}, {spec_3.shape}")
+
+    return {"specs": (spec_1, spec_2, spec_3), "duration": 4.0}
 
 def time_method(specs, method, num_iter=5, **kwargs):
     total_time = 0.0
@@ -26,33 +28,35 @@ def time_method(specs, method, num_iter=5, **kwargs):
 
 def time_all_pipeline(num_iter):
 
-    specs = load_test_signal()
+    test_sig_dict = load_test_signal()
+    specs = test_sig_dict["specs"]
+    duration = test_sig_dict["duration"]
 
     print("Execution time for each method and implementation:")
 
     print("\n==== Binwise minimum ====\n")
     _, average_time_ctfr = time_method(specs, method='min', num_iter=num_iter)
-    print(f"ctfr: {average_time_ctfr:0.3f} s")
+    print(f"ctfr: {average_time_ctfr:0.3f} s -- {100*average_time_ctfr/duration:0.2f}% real-time")
     
     print("\n==== SWGM ====\n")
     cspec_base, average_time_base = time_method(specs, method='baseline_swgm', num_iter=num_iter)
-    print(f"Baseline: {average_time_base:0.3f} s")
+    print(f"Baseline: {average_time_base:0.3f} s -- {100*average_time_base/duration:0.2f}% real-time")
     cspec_ctfr, average_time_ctfr = time_method(specs, method='swgm', num_iter=num_iter)
-    print(f"ctfr: {average_time_ctfr:0.3f} s")
+    print(f"ctfr: {average_time_ctfr:0.3f} s -- {100*average_time_ctfr/duration:0.2f}% real-time")
     assert np.allclose(cspec_base, cspec_ctfr)
 
     print("\n==== FLS ====\n")
     cspec_base, average_time_base = time_method(specs, method='baseline_fls', num_iter=num_iter)
-    print(f"Baseline: {average_time_base:0.3f} s")
+    print(f"Baseline: {average_time_base:0.3f} s -- {100*average_time_base/duration:0.2f}% real-time")
     cspec_ctfr, average_time_ctfr = time_method(specs, method='fls', num_iter=num_iter)
-    print(f"ctfr: {average_time_ctfr:0.3f} s")
+    print(f"ctfr: {average_time_ctfr:0.3f} s -- {100*average_time_ctfr/duration:0.2f}% real-time")
     assert np.allclose(cspec_base, cspec_ctfr)
 
     print("\n==== LT ====\n")
     cspec_base, average_time_base = time_method(specs, method='baseline_lt', num_iter=num_iter)
-    print(f"Baseline: {average_time_base:0.3f} s")
+    print(f"Baseline: {average_time_base:0.3f} s -- {100*average_time_base/duration:0.2f}% real-time")
     cspec_ctfr, average_time_ctfr = time_method(specs, method='lt', num_iter=num_iter)
-    print(f"ctfr: {average_time_ctfr:0.3f} s")
+    print(f"ctfr: {average_time_ctfr:0.3f} s -- {100*average_time_ctfr/duration:0.2f}% real-time")
     assert np.allclose(cspec_base, cspec_ctfr)
 
 if __name__ == "__main__":
