cf224f3527211c4635da778712a9ebe6cfbef16b
Bernardo Boechat <bernardoant@poli.ufrj.br>
2025-03-27 20:22:48 -0300

    Update parameters doc generator to document SLS.

--- docs/combination_methods/sls/calling.rst
+++ docs/combination_methods/sls/calling.rst
@@ -4,10 +4,10 @@ Calling signature
 .. function:: ctfr.methods.sls_h(signal, sr, *, <shared parameters>, lek, lsk, lem, lsm, beta, energy_criterium_db)
    :noindex:
 
-.. function:: ctfr.methods.sls_i(signal, sr, *, <shared parameters>, lek, lsk, lem, lsm, beta, interp_steps)
+.. function:: ctfr.methods.sls_h_from_specs(specs, *, <shared parameters>, lek, lsk, lem, lsm, beta, energy_criterium_db)
    :noindex:
 
-.. function:: ctfr.methods.sls_h_from_specs(specs, *, <shared parameters>, lek, lsk, lem, lsm, beta, energy_criterium_db)
+.. function:: ctfr.methods.sls_i(signal, sr, *, <shared parameters>, lek, lsk, lem, lsm, beta, interp_steps)
    :noindex:
 
 .. function:: ctfr.methods.sls_i_from_specs(specs, *, <shared parameters>, lek, lsk, lem, lsm, beta, interp_steps)
@@ -21,4 +21,31 @@ See :func:`ctfr.ctfr` and :func:`ctfr.ctfr_from_specs` for more details on the s
 Parameters
 ~~~~~~~~~~
 
-Placeholder.
+**lsm** (`int > 0, odd, optional`)
+
+   Width in time frames of the analysis window used in the local sparsity computation. Defaults to 11.
+
+**beta** (`float >= 0, optional`)
+
+   Factor used in the computation of combination weights. Defaults to 0.3.
+
+**lsk** (`int > 0, odd, optional`)
+
+   Width in frequency bins of the analysis window used in the local sparsity computation. Defaults to 21.
+
+**lek** (`int > 0, odd, optional`)
+
+   Width in frequency bins of the analysis window used in the local energy computation. Defaults to 21.
+
+**lem** (`int > 0, odd, optional`)
+
+   Width in time frames of the analysis window used in the local energy computation. Defaults to 11.
+
+**energy_criterium_db** (`float, optional`)
+
+   Local energy criterium (in decibels) that distinguishes high-energy regions (where LS is computed) from low-energy regions (where binwise minimum is computed). Defaults to -40. **Specific to sls_h**.
+
+**interp_steps** (`ndarray of int, shape P x 2, optional`)
+
+   Interpolation steps to use when computing the local sparsity. interp_steps[p, i] refers to the interpolation step of axis i (frequency is 0, time is 1) for spectrogram p. When calling :func:`ctfr.ctfr` (or :func:`ctfr.methods.sls_i`), ``interp_steps[p]`` defaults to ``[n_fft // l, l // (2 * hop_length)]``. When calling :func:`ctfr.ctfr_from_specs` (or :func:`ctfr.methods.sls_i_from_specs`), this argument must the provided by the user. **Specific to sls_i**.
+
--- docs/conf_extras/combination_methods_doc.py
+++ docs/conf_extras/combination_methods_doc.py
@@ -1,5 +1,7 @@
 import os
 from ctfr import _methods_dict
+from operator import iand
+from functools import reduce
 
 class CombinationMethodsDoc:
 
@@ -57,11 +59,11 @@ class CombinationMethodsDoc:
                     f.write(self.doc_parameter(parameter) + "\n\n")
 
 class CombinationMethodsWithVariantsDoc(CombinationMethodsDoc):
-    # TODO implement this class if more methods with variants are added.
 
     def __init__(
         self, 
-        method_keys, 
+        method_keys_iter,
+        output_key,
         page_title="Calling signature", 
         parameters_subtitle="Parameters", 
         combination_methods_doc_folder="combination_methods", 
@@ -69,22 +71,80 @@ class CombinationMethodsWithVariantsDoc(CombinationMethodsDoc):
     ):
         self.init_defaults(page_title, parameters_subtitle, combination_methods_doc_folder, file_name)
 
-        self.method_keys_iter = list(method_keys)
+        self.method_keys_iter = list(method_keys_iter)
         self.method_entries_map = {key: _methods_dict[key] for key in self.method_keys_iter}
-        self.parameter_names_map = {key: list(self.method_entries_map[key].get("parameters", {}).keys()) for key in self.method_keys_iter}
-        self.output_path = os.path.join(self.combination_methods_doc_folder, self.method_key, self.file_name)
+        self.output_path = os.path.join(self.combination_methods_doc_folder, output_key, self.file_name)
 
-    def get_parameter_names_map(self):
-        pass
+        self.initialize_parameters()
+
+    def initialize_parameters(self):
+        # Dict of method_key: list of parameter names
+        self.parameter_names_map = {method_key: list(self.method_entries_map[method_key].get("parameters", {}).keys()) for method_key in self.method_keys_iter}
+
+        params_list_list = list(self.parameter_names_map.values())
+        # List of shared parameter names
+        self.shared_parameters_keys = list(set(params_list_list[0]).intersection(*params_list_list[1:]))
+
+
+    def doc_parameter_shared(self, parameter):
+        parameter_entry = self.method_entries_map[self.method_keys_iter[0]]["parameters"][parameter] # Uses type_and_info and description from the first method key. (They should be the same for all method keys)
+        first_line = f"**{parameter}** (`{parameter_entry['type_and_info']}, optional`)"
+        second_line = f"   {parameter_entry['description']}"
+        return first_line + "\n\n" + second_line
+
+    def doc_parameter_specific(self, parameter, method_key):
+        parameter_entry = self.method_entries_map[method_key]["parameters"][parameter]
+        first_line = f"**{parameter}** (`{parameter_entry['type_and_info']}, optional`)"
+        second_line = f"   {parameter_entry['description']}" + f" **Specific to {method_key}**."
+        return first_line + "\n\n" + second_line
+
+    def signature_end(self, method_key):
+        if self.parameter_names_map[method_key]:
+            segment = ", " + ", ".join(self.parameter_names_map[method_key])
+        else:
+            segment = ""
+        return segment + ")"
+
+    def run(self, verbose=False):
+        if verbose:
+            print(f"Documenting {self.method_keys_iter} parameters to {self.output_path}")
+        with open(self.output_path, "w") as f:
+            f.write(self.page_title + "\n")
+            f.write("-" * len(self.page_title) + "\n\n")
+            for method_key in self.method_keys_iter:
+                f.write(f'.. function:: ctfr.methods.{method_key}(signal, sr, *, <shared parameters>')
+                f.write(self.signature_end(method_key) + "\n" + "   :noindex:" + "\n\n")
+                f.write(f'.. function:: ctfr.methods.{method_key}_from_specs(specs, *, <shared parameters>')
+                f.write(self.signature_end(method_key) + "\n" + "   :noindex:" + "\n\n")
+            f.write(f'.. note::' + "\n" + "   " + "As with all combination methods, you can also use :func:`ctfr.ctfr` or :func:`ctfr.ctfr_from_specs`." "\n\n")
+            f.write("See :func:`ctfr.ctfr` and :func:`ctfr.ctfr_from_specs` for more details on the shared parameters for computing CTFRs with this package.")
+            if self.shared_parameters_keys or any(self.parameter_names_map.values()):
+                f.write(" The parameters specific to this method (passed as keyword arguments) are described below." + "\n\n")
+                f.write(self.parameters_subtitle + "\n")
+                f.write("~" * len(self.parameters_subtitle) + "\n\n")
+                for parameter in self.shared_parameters_keys:
+                    f.write(self.doc_parameter_shared(parameter) + "\n\n")
+                for method_key in self.method_keys_iter:
+                    for parameter in self.parameter_names_map[method_key]:
+                        if parameter not in self.shared_parameters_keys:
+                            f.write(self.doc_parameter_specific(parameter, method_key) + "\n\n")
+                
+
+        
         
 
 def combination_methods_doc_main():
 
     exclude_methods_from_param_doc = [
-        "mean", "hmean", "gmean", "min", "sls_i", "sls_h"
+        "mean", "hmean", "gmean", "min", "sls_h", "sls_i"
     ]
 
+    sls_group = ["sls_h", "sls_i"]
+
     for method_key in _methods_dict.keys():
-        if method_key not in exclude_methods_from_param_doc:
+        if method_key not in exclude_methods_from_param_doc and method_key not in sls_group:
             doc = CombinationMethodsDoc(method_key)
             doc.run(verbose=True)
+
+    doc = CombinationMethodsWithVariantsDoc(sls_group, output_key="sls")
+    doc.run(verbose=True)
